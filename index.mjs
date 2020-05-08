833e9ec6ba7d641c47d008b78a8c16c0717edb4e
Victor Mus <sdwvit@inet.ua>
2020-05-08 19:13:12 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -14,7 +14,7 @@ server.post('/', async (request, reply) => {
 
 const start = async () => {
   try {
-    await server.listen(PORT);
+    await server.listen(PORT, '0.0.0.0');
   } catch (error) {
     server.log.error(error);
     process.exit(1);
